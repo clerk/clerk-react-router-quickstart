cef40c549ebe5b0b33a05a2264fc984a1c78c439
Alexis Aguilar <98043211+alexisintech@users.noreply.github.com>
2025-10-07 17:20:52 -0400

    update config comments

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,10 +1,12 @@
-import type { Config } from "@react-router/dev/config";
+import type { Config } from '@react-router/dev/config'
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
+  // Config options
+
+  // Server-side render by default
+  // To enable SPA mode, set this to `false`
   ssr: true,
   future: {
     v8_middleware: true,
   },
-} satisfies Config;
+} satisfies Config
